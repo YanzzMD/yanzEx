5ce0b2d27424298cfc88391d2e64d247db1b0e97
YanzzMD <saturnuus7@gmail.com>
2024-08-30 18:19:47 +0700

    unzipgit

---
+++ yanz.sh
@@ -0,0 +1,27 @@
+echo "installing fake ip"
+sleep 3
+echo "tunggu bentar"
+sleep 2
+echo " ■■■□□□□□□□□ 20% "
+echo
+sleep 1
+echo " ■■■■■□□□□□□ 40% "
+echo
+sleep 1
+echo " ■■■■■■■□□□□ 60% "
+echo
+sleep 1
+echo " ■■■■■■■■■□□ 80% "
+echo
+sleep 1
+echo " ■■■■■■■■■■■ 100% "
+echo 
+echo "udah bang, bentar ambil ipnya"
+sleep 4
+echo "nih bang ipnya [192.168.0.0]"
+sleep 1
+echo "running script"
+sleep 1
+echo "Created By YanzzOfficial"
+sleep 1
+bash camphish.sh
